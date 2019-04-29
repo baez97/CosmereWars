cfc9c12d4d66240208d5294716bbd89d63e999a1
Bernardo <Bernardo.Martinez1@alu.uclm.es>
2019-04-29 18:39:58 +0200

    Fix merge errors

---
+++ Assets/Scripts/SpellsCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellsCamera : MonoBehaviour
+{
+    public GameObject mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = mainCamera.transform.position;
+        transform.rotation = mainCamera.transform.rotation;
+        Debug.Log("Asds");
+        
+    }
+}
