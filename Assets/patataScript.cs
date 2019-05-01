1dcf9e6c3a6143c6639f81a64ad28297433642bc
Guillermo Fernandez <folez97@gmail.com>
2019-05-01 21:29:39 +0200

    Elantris Preface added

---
+++ Assets/patataScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patataScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Holi");
+    }
+}
