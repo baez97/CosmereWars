94602cd9511db6efdac6179f00897917e489c815
Bernardo <bernardo-martinez@live.com>
2019-05-05 23:19:41 +0200

    Enemies on Rosard

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -89,6 +89,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 TocuhpadTouch
+        {
+            get
+            {
+                return SteamVR_Actions.default_TocuhpadTouch;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
