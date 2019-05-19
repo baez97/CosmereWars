c749ef4711cede1f0ecadd3f97c7a68ba211bcf9
Bernardo <bernardo-martinez@live.com>
2019-05-19 15:47:23 +0200

    Testing faults

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public int lives = 1;
+    public GameObject room;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Player Controller running");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Player Controller running");
+    }
+
+    void OnCollisionEnter (Collision col)
+    {
+        if(col.gameObject.tag == "Enemy")
+        {
+            lives--;
+            Debug.Log("You've been hit: Lives " + lives);
+            if(lives <=0) {
+                Scene scene = SceneManager.GetActiveScene();
+                Destroy(room);
+                SceneManager.LoadScene(scene.name);
+            }
+        }
+    }
+}
